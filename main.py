c643d709975d6fb208a86b5ec03de8f97769f62f
miohitokiri5474 <lltzpp>
2023-10-27 23:30:14 +0800

    finish 2.1Guassian Blur

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
 import sys
 
 import cv2
-
 import numpy as np
-
 from PyQt5.QtWidgets import (
     QApplication,
+    QFileDialog,
     QHBoxLayout,
     QLabel,
     QLineEdit,
     QPushButton,
     QVBoxLayout,
     QWidget,
-    QFileDialog,
 )
 
 file_name = None
 image1 = None
 image2 = None
+
+def update_radius(value):
+    global window_radius
+    window_radius = value
 
 
 def main():
@@ -158,24 +160,24 @@ def main():
     # General
     def get_path():
         global file_name
-        file_name = QFileDialog.getOpenFileName ( None, 'open file', '.' )[0]
+        file_name = QFileDialog.getOpenFileName(None, "open file", ".")[0]
 
     # For load images over all
     def load_img1_btn_clicked():
         global image1
         get_path()
-        image1 = cv2.imread ( file_name )
+        image1 = cv2.imread(file_name)
         if image1 is None:
-            print ( "[ERROR]: Image cannot load" )
+            print("[ERROR]: Image cannot load")
         else:
             print("Loaded Image 1", file_name)
 
     def load_img2_btn_clicked():
         global image2
         get_path()
-        image2 = cv2.imread ( file_name )
+        image2 = cv2.imread(file_name)
         if image2 is None:
-            print ( "[ERROR]: Image cannot load" )
+            print("[ERROR]: Image cannot load")
         else:
             print("Loaded Image 2", file_name)
 
@@ -183,54 +185,77 @@ def main():
     def Block1_btn_1_1_clicked():
         print("Color Separation button clicked")
         if image1 is None:
-            print ( "[ERROR]: Please load image first" )
+            print("[ERROR]: Please load image first")
             return
 
-        b, g, r = cv2.split ( image1 )
+        b, g, r = cv2.split(image1)
 
-        zeros = np.zeros_like ( b )
+        zeros = np.zeros_like(b)
 
-        cv2.imshow ( "Blue Image", cv2.merge ( ( b, zeros, zeros ) ) )
-        cv2.imshow ( "Green Image", cv2.merge ( ( zeros, g, zeros ) ) )
-        cv2.imshow ( "Red Image", cv2.merge ( ( zeros, zeros, r ) ) )
+        cv2.imshow("Blue Image", cv2.merge((b, zeros, zeros)))
+        cv2.imshow("Green Image", cv2.merge((zeros, g, zeros)))
+        cv2.imshow("Red Image", cv2.merge((zeros, zeros, r)))
 
-        cv2.waitKey ( 0 )
+        cv2.waitKey(0)
         cv2.destroyAllWindows()
 
     def Block1_btn_1_2_clicked():
         print("Color Transformation button clicked")
         if image1 is None:
-            print ( "[ERROR]: Please load image first" )
+            print("[ERROR]: Please load image first")
             return
 
-        b, g, r = cv2.split ( image1 )
-        avg = ( r + g + b ) // 3
+        b, g, r = cv2.split(image1)
+        avg = (r + g + b) // 3
 
-        cv2.imshow ( "OpenCV function", cv2.cvtColor ( image1, cv2.COLOR_BGR2GRAY ) )
-        cv2.imshow ( "Average weighted", cv2.merge ( ( avg, avg, avg ) ) )
+        cv2.imshow("OpenCV function", cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY))
+        cv2.imshow("Average weighted", cv2.merge((avg, avg, avg)))
 
-        cv2.waitKey ( 0 )
+        cv2.waitKey(0)
         cv2.destroyAllWindows()
 
     def Block1_btn_1_3_clicked():
         print("Color Extraction button clicked")
 
         if image1 is None:
-            print ( "[ERROR]: Please load image first" )
+            print("[ERROR]: Please load image first")
             return
 
-        hsv_img = cv2.cvtColor ( image1, cv2.COLOR_BGR2HSV )
-        mask = cv2.bitwise_not ( cv2.inRange ( hsv_img, np.array ( [15, 25, 25] ), np.array ( [85, 255, 255] ) ) )
+        hsv_img = cv2.cvtColor(image1, cv2.COLOR_BGR2HSV)
+        mask = cv2.bitwise_not(
+            cv2.inRange(hsv_img, np.array([15, 25, 25]), np.array([85, 255, 255]))
+        )
 
-        cv2.imshow ( "I1 mask", cv2.bitwise_not ( mask ) )
-        cv2.imshow ( "Extracted Color", cv2.bitwise_and ( image1, image1, mask = mask ) )
+        cv2.imshow("I1 mask", cv2.bitwise_not(mask))
+        cv2.imshow("Extracted Color", cv2.bitwise_and(image1, image1, mask=mask))
 
-        cv2.waitKey ( 0 )
+        cv2.waitKey(0)
         cv2.destroyAllWindows()
 
     # For Block2
     def Block2_btn_2_1_clicked():
         print("Gaussian blur button clicked")
+        
+        if image1 is None:
+            print ( "[ERROR]: Please load image first" )
+            return
+
+        cv2.namedWindow ( 'Gaussian Blur' )
+        cv2.createTrackbar ( 'm:', 'Gaussian Blur', 1, 5, update_radius )
+
+        while True:
+            if cv2.waitKey ( 1 ) & 0xFF == ord ( 'q' ):
+                cv2.destroyAllWindows()
+                break
+
+            img = image1
+            current_radius = cv2.getTrackbarPos ( 'm:', 'Gaussian Blur' )
+
+            sz = ( 2 * current_radius + 1, 2 * current_radius + 1 )
+
+            img = cv2.GaussianBlur ( image1, sz, 0 )
+            cv2.imshow ( 'Gaussian Blur', img )
+
 
     def Block2_btn_2_2_clicked():
         print("Bilateral blur button clicked")
